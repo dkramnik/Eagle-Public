25d5385fd3483dd7a1b80a18984b04662f64ba5f
dkramnik <kramnik@mit.edu>
2018-08-04 12:11:37 -0400

    Added code for COM port auto-detect

--- meng-plant-raman/MATLAB/controller-rev2/class_test.m
+++ meng-plant-raman/MATLAB/controller-rev2/class_test.m
@@ -4,8 +4,26 @@ close all
 clear
 clc
 
+input( 'Unplug leaf heater controller, then press [enter].' );
+
+before = seriallist;
+
+input( 'Replug leaf heater controller, then press [enter] to start.' );
+
+after = seriallist;
+while( isempty( setdiff( after, before ) ) )    % Wait for new device
+    after = seriallist;
+    pause( 0.1 );
+end
+port_name = setdiff( after, before );
+
+if length( port_name ) > 1
+    port_name = port_name( 1 ); % OSX systems: 'cu' comes before 'tty'
+end
+
 % use 'seriallist' command to list all available serial ports
-sys_heater = leaf_heater_system( [ ] );    % Search for default OSX inst. name
+% if you know the port name, you can skip the steps above
+sys_heater = leaf_heater_system( port_name );    % Search for default OSX inst. name
 
 sys_heater.set_temp_setpoint( 50 );
 sys_heater.control_loop_start( );
